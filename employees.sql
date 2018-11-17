a5c16437f48140cff30f2a785656b42454dd309b
Xavier Faus i Torà <xfaus@xtec.cat>
2018-11-17 13:23:39 +0100

    Update employees.sql

--- employees.sql
+++ employees.sql
@@ -1,7 +1,7 @@
 PRAGMA foreign_keys=OFF;
 BEGIN TRANSACTION;
-CREATE TABLE employees (id integer primary key, name text);
-INSERT INTO "employees" VALUES(1,'Tom…s');
+CREATE TABLE employees (id integer , name text, primary key(id));
+INSERT INTO "employees" VALUES(1,'Tomâ€¦s');
 INSERT INTO "employees" VALUES(2,'David Llordes');
 INSERT INTO "employees" VALUES(3,'Ivan Ripoll');
 INSERT INTO "employees" VALUES(4,'Gerard Molina');
